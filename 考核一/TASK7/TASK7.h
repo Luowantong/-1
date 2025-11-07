3e887ea392bc102f719f79fa431e604ef9e87877
Luowantong <2826223614@qq.com>
2025-11-08 00:01:47 +0800

    Delete 考核一 directory

--- 考核一/TASK7/TASK7.h
+++
@@ -1,8 +0,0 @@
-#define TASK7_H
-#include<cstring>
-using namespace std;
-struct Interface
-{
-    char data[100];
-    int id;
-};
--- 考核一/task2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if ! command -v espeak &> /dev/null;then
-sudo apt install -y espeak
-fi
-espeak "fuck you,ha ha ha dashabi!"
-echo "ben dan bei wo shua le ba"
